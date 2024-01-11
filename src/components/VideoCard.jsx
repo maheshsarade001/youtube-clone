09c05356971360fe91cade3d7199a4ecfa87512f
maheshsarade001 <maheshsarade001@gmail.com>
2024-01-11 21:23:00 +0530

    VideoCard component finished

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,6 +17,35 @@ const VideoCard = ({ video }) => {
           />
           {video?.lengthSeconds && <VideoLength time={video?.lengthSeconds} />}
         </div>
+        <div className="flex text-white mt-3">
+          <div className="flex items-start">
+            <div className="flex h-9 w-9 rounded-full overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src={video?.author?.avatar[0]?.url}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="flex flex-col ml-3 overflow-hidden">
+            <span className="text-sm font-bold line-clamp-2">
+              {video?.title}
+            </span>
+            <span className="text-xs font-semibold mt-2 text-white/70 flex items-center">
+              {video?.author?.title}
+              {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
+                <bsFillCheckCircleFill className="text-white/50 text-xs ml-1" />
+              )}
+            </span>
+            <div className="flex text-xs font-semibold text-white/70 truncate overflow-hidden">
+              <span>{`${abbreviateNumber(video?.stats?.views, 2)} views`}</span>
+              <span className="flex text-2xl leading-none font-bold text-white/70 relative top-[-10px] mx-1">
+                .
+              </span>
+              <span className="truncate">{video?.publishedTimeText}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </Link>
   );
